168fe7953dff33f96f31d8c2468d615f25b8efd1
PagiY <pagiyap003@gmail.com>
2023-01-05 20:22:31 +0800

    Updated project name from "weatherapp" to "weatherproject"

--- weatherproject/weatherproject/urls.py
+++ weatherproject/weatherproject/urls.py
@@ -1,4 +1,4 @@
-"""weatherapp URL Configuration
+"""weatherproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("mainapp/", include("mainapp.urls"))
+    path("", include("mainapp.urls"))
 ]
